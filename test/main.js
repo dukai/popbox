a882f5c9f517529e081c8bfbb6df108aadc0858c
dukai <dukai86@gmail.com>
2016-07-28 17:25:23 +0800

    修改触发回调与隐藏动作的顺序 toast添加是否显示cover的开关

--- test/main.js
+++ test/main.js
@@ -5,22 +5,25 @@ requirejs.config({
     dtools: 'test/lib/dtools/tools-1.0.3.min',
     template: 'test/lib/template/template',
     jquery: 'test/lib/jquery/dist/jquery.min',
-    popbox: 'popbox-0.1.0.min'
+    popbox: 'popbox'
   }
 });
 
 require(['popbox'], function(popbox){
 	// popbox.toast('正在加载中...', 3e3);
+  popbox.setDesktopDefault();
+  popbox.Toast.SHOW_COVER = true;
+  // popbox.toast('aaaaa', 3e3);
 
   popbox.confirm("test", function(){
     popbox.alert('aaa', function(){
       popbox.toast('<i class="ico ico-success"></i>正在加载中...', 3e3, function(){
-        popbox.alert('加载完成', function(){
-          popbox.toast('<i class="ico ico-success"></i>真的是加载完成了').hideLoader();
-          setTimeout(function(){
-            popbox.toast().hide();
-          }, 20e3);
-        });
+      //   popbox.alert('加载完成', function(){
+      //     popbox.toast('<i class="ico ico-success"></i>真的是加载完成了').hideLoader();
+      //     setTimeout(function(){
+      //       popbox.toast().hide();
+      //     }, 20e3);
+      //   });
       });
     });
   });
